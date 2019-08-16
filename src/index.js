e7e74b7afe5202b0f8127c0fc0b6f37fa0af0c72
sebug <sebastian.gfeller@gmail.com>
2019-08-16 10:32:31 +0200

    Executed webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import * as d3 from "d3";
+
 function hello() {
     const element = document.createElement('div');
     element.innerHTML = 'Hello for D3';
+    console.log(d3);
     return element;
 }
 
