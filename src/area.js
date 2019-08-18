c9248ad030fd515afc9a6b786649e2b3c354fcbf
sebug <sebastian.gfeller@gmail.com>
2019-08-18 16:55:34 +0200

    Correct X axis

--- src/area.js
+++ src/area.js
@@ -3,22 +3,17 @@ import css from './style.css';
 
 function areaOuter() {
     let randomData = [];
-    let minY = 200;
-    let maxY = 0;
     for (let i = 0; i < 17; i += 1) {
 	const randomValue = Math.round(Math.random() * 200);
-	if (randomValue > maxY) {
-	    maxY = randomValue;
-	}
-	if (randomValue < minY) {
-	    minY = randomValue;
-	}
 	randomData.push(randomValue);
     }
     let years = [];
     for (let i = 0; i < 17; i += 1) {
 	years.push((2000 + i).toString());
     }
+
+    const parseDate = d3.timeParse('%Y');
+    
     const height = 200;
     const width = 500;
 
@@ -30,16 +25,22 @@ function areaOuter() {
     };
 
     const y = d3.scaleLinear()
-	  .domain([0, maxY])
+	  .domain([0, d3.max(randomData)])
 	  .range([height, 0]);
+
+    const x = d3.scaleTime()
+	  .domain(d3.extent(years, parseDate))
+	  .range([0, width]);
 
     const yAxis = d3.axisLeft(y)
 	  .ticks(3)
 	  .tickPadding(10)
 	  .tickSize(10);
 
+    const xAxis = d3.axisBottom(x);
+
     const area = d3.area()
-	  .x((d, i) => i * 20)
+	  .x((d, i) => x(parseDate(years[i])))
 	  .y0(height)
 	  .y1(y);
 
@@ -54,6 +55,10 @@ function areaOuter() {
 
     chartGroup.append('g').attr('class','axis y')
 	.call(yAxis);
+
+    chartGroup.append('g').attr('class', 'axis x')
+	.attr('transform', `translate(0, ${height})`)
+	.call(xAxis);
 }
 
 export default areaOuter;
