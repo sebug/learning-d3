ba1bc370499c50ecee2d9437d504e30cfd7681f7
sebug <sebastian.gfeller@gmail.com>
2019-08-18 09:44:55 +0200

    With scale

--- src/area.js
+++ src/area.js
@@ -3,8 +3,17 @@ import css from './style.css';
 
 function areaOuter() {
     let randomData = [];
+    let minY = 200;
+    let maxY = 0;
     for (let i = 0; i < 17; i += 1) {
-	randomData.push(Math.round(Math.random() * 200));
+	const randomValue = Math.round(Math.random() * 200);
+	if (randomValue > maxY) {
+	    maxY = randomValue;
+	}
+	if (randomValue < minY) {
+	    minY = randomValue;
+	}
+	randomData.push(randomValue);
     }
     let years = [];
     for (let i = 0; i < 17; i += 1) {
@@ -13,10 +22,14 @@ function areaOuter() {
     const height = 200;
     const width = 500;
 
+    const y = d3.scaleLinear()
+	  .domain([0, maxY])
+	  .range([height, 0]);
+
     const area = d3.area()
 	  .x((d, i) => i * 20)
 	  .y0(height)
-	  .y1((d, i) => height - d);
+	  .y1(y);
 
     const svg = d3.select('body').append('svg')
 	  .attr('width', '100%')
