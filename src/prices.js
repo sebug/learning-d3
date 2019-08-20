afcf8d88ea43e9186661ec2ed8b2f0bf8e1dc419
sebug <sebastian.gfeller@gmail.com>
2019-08-20 10:46:30 +0200

    With parsing

--- src/prices.js
+++ src/prices.js
@@ -2,12 +2,19 @@ import * as d3 from "d3";
 import css from './style.css';
 import pricesCSV from '../prices.csv';
 
+const parseDate = d3.timeParse('%m/%d/%Y');
+
 function prices() {
     const svg = d3.select('body').append('svg')
 	  .attr('height','100%')
 	  .attr('width','100%');
 
-    const parsed = d3.csvParse(pricesCSV);
+    const parsed = d3.csvParse(pricesCSV, (d) => {
+	return {
+	    month: parseDate(d.month),
+	    price: Number(d.price.trim().slice(1))
+	};
+    });
 
     console.log(parsed);
 }
