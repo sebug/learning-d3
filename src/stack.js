470c544ba3c544b8d87d97ca90d182a543383ecc
sebug <sebastian.gfeller@gmail.com>
2019-08-20 21:26:23 +0200

    Massaging data a bit more

--- src/stack.js
+++ src/stack.js
@@ -3,10 +3,22 @@ import css from './style.css';
 import theXML from '../data2.xml';
 
 function stack() {
+    const parseDate = d3.timeParse('%Y');
+    
     const domParser = new DOMParser();
     const parsedXml = domParser.parseFromString(theXML, 'text/xml');
 
-    console.log(parsedXml);
+    const dataXml = Array.from(parsedXml.querySelectorAll('dat'))
+	  .map(d => {
+	      return {
+		  date: parseDate(d.getAttribute('id')),
+		  top: Number(d.querySelector('top').textContent),
+		  middle: Number(d.querySelector('middle').textContent),
+		  bottom: Number(d.querySelector('bottom').textContent)
+	      };
+	  });
+
+    console.log(dataXml);
 }
 
 export default stack;
