6bcca7a57500e95fcaee3db7a56e1e4f427ff881
sebug <sebastian.gfeller@gmail.com>
2019-08-20 14:43:46 +0200

    The nodes

--- src/tree.js
+++ src/tree.js
@@ -3,7 +3,37 @@ import css from './style.css';
 import theJSON from '../treeData.json';
 
 function tree() {
-    console.log(theJSON);
+    const height = 200;
+    const width = 500;
+    const margin = {
+	top: 40,
+	left: 50,
+	right: 50,
+	bottom: 0
+    };
+    const treeLayout = d3.tree().size([width, height]);
+
+    const svg = d3.select('body').append('svg')
+	  .attr('width', '100%')
+	  .attr('height', '100%');
+
+
+    const chartGroup = svg.append('g')
+	  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    const root = d3.hierarchy(theJSON[0]);
+
+    treeLayout(root);
+
+    chartGroup.selectAll('circle')
+	.data(root.descendants())
+	.enter()
+	.append('circle')
+	.attr('cx', (d, i) => d.x)
+	.attr('cy', (d, i) => d.y)
+	.attr('r', 5);
+
+    console.log(root);
 }
 
 export default tree;
